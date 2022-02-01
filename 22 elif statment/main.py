9ec39150247be2f9060cb955dd5574db1c6f41e8
arifwonk <lunix.arif@gmail.com>
2022-02-01 22:53:55 +0700

    Update 1

---
+++ 22 elif statment/main.py
@@ -0,0 +1,14 @@
+# ELIF = el if statement
+
+nama = input("Nama Kamu siapa ? ").lower() # paksa ke lower character
+
+if nama == "rafka": # kondisi 1
+    print("Hi ganteng BangeetttzzZZ") # aksi true 1
+elif nama == "rival": # kondisi 2
+    print("Wah Kece sekali...") # aksi true 2
+elif nama == "arif": # kondisi 3
+    print("Wih keyeennn abbiiieezzzZZZ") # aksi true 3
+else: 
+    print("Au Ahh...Gak kenal") # aksi False
+print("ini adalah akhir dari program")
+
