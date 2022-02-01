72825bb8282b2121427f7796b9b96676a5d875c6
arifwonk <lunix.arif@gmail.com>
2022-02-01 22:33:19 +0700

    Update 1

---
+++ 21 if dan else statment/main.py
@@ -0,0 +1,28 @@
+# if dan else statement
+
+# 1. if nya
+# 2. kondisinya
+# 3. Aksinya
+
+nama = input("Siapa nama anda? ").lower() # paksa ke lower character
+
+# Program sebelumnya
+# if kondisi aksi
+# program selanjutnya
+
+# 1. Program if inline
+# if nama == "rafka": print("kamu ganteng banget....!!!")
+# print(f"Terima kasih {nama}")
+
+# 2. Program if indentation
+# if nama == "ucup":
+#     print("wadidaw...ganteng AbbiizzzzzZZZ")
+#     print("Kamu juga ganteng KOK")
+# print(f"terimakasih {nama}")
+
+# 3. else statement
+if nama == "rafka":
+    print("Hai rafka keren banget...!")
+else:
+    print("Kamu bukan rafka, gak keren Lah..wkwk")
+print("End Program")
